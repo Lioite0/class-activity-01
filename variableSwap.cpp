904e5eafb4e5269cfd84b2cf1892d248c7f03cfc
Lioite h <elliottrlawrence@gmail.com>
2025-01-27 11:06:01 -0800

    created variable swap function

---
+++ variableSwap.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ellio on 1/27/2025.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 5;
+   int  b = 2;
+    swap(a, b);
+    return 0;
+  }
+
+ void swap(int& a, int& b){
+   int c = a;
+   a = b;
+   b = c;
+ }
